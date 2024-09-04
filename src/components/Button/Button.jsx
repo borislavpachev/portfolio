f0ceadc300a88384014250aa549ab86030bb4412
borislavpachev <borislav.pachev@gmail.com>
2024-09-05 01:45:46 +0300

    .

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function Button({
+  children,
+  onClick,
+  className,
+  type = 'button',
+}) {
+  return (
+    <button
+      type={type}
+      className={`${className} text-lg px-8 py-2 rounded bg-white/20 border border-blue-950 text-blue-950
+        hover:bg-blue-950 hover:shadow-xl hover:shadow-blue-300/40 hover:text-white transition duration-200 ease-in-out`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  type: PropTypes.string,
+};
