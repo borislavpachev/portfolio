e548b5ec3748b8e58ef4cca229b9e7f9a1d45b85
borislavpachev <borislav.pachev@gmail.com>
2024-09-05 01:42:54 +0300

    utils/constants for tech stack and projects

---
+++ src/utils/constants.js
@@ -0,0 +1,150 @@
+export const techStackData = [
+  {
+    id: 'javascript',
+    name: 'JavaScript',
+    logo: '/icons/javascript.svg',
+    color:
+      'brightness(0) saturate(100%) invert(83%) sepia(89%) saturate(1413%) hue-rotate(335deg) brightness(97%) contrast(100%)',
+  },
+  {
+    id: 'typescript',
+    name: 'TypeScript',
+    logo: '/icons/typescript.svg',
+    color:
+      'brightness(0) saturate(100%) invert(43%) sepia(44%) saturate(880%) hue-rotate(171deg) brightness(91%) contrast(89%)',
+  },
+  {
+    id: 'react',
+    name: 'React',
+    logo: '/icons/react.svg',
+    color:
+      'brightness(0) saturate(100%) invert(60%) sepia(86%) saturate(455%) hue-rotate(173deg) brightness(114%) contrast(97%)',
+  },
+  {
+    id: 'html5',
+    name: 'HTML5',
+    logo: '/icons/html5.svg',
+    color:
+      'brightness(0) saturate(100%) invert(39%) sepia(31%) saturate(4145%) hue-rotate(348deg) brightness(93%) contrast(92%)',
+  },
+  {
+    id: 'css3',
+    name: 'CSS3',
+    logo: '/icons/css3.svg',
+    color:
+      'brightness(0) saturate(100%) invert(27%) sepia(74%) saturate(2027%) hue-rotate(187deg) brightness(94%) contrast(84%)',
+  },
+  {
+    id: 'sass',
+    name: 'Sass',
+    logo: '/icons/sass.svg',
+    color:
+      'brightness(0) saturate(100%) invert(49%) sepia(32%) saturate(769%) hue-rotate(277deg) brightness(96%) contrast(88%)',
+  },
+  {
+    id: 'tailwindcss',
+    name: 'Tailwind CSS',
+    logo: '/icons/tailwindcss.svg',
+    color:
+      'brightness(0) saturate(100%) invert(56%) sepia(100%) saturate(2341%) hue-rotate(149deg) brightness(96%) contrast(95%)',
+  },
+  {
+    id: 'bootstrap',
+    name: 'Bootstrap',
+    logo: '/icons/bootstrap.svg',
+    color:
+      'brightness(0) saturate(100%) invert(43%) sepia(87%) saturate(405%) hue-rotate(222deg) brightness(75%) contrast(95%)',
+  },
+  {
+    id: 'gsap',
+    name: 'GSAP',
+    logo: '/icons/gsap.svg',
+  },
+  {
+    id: 'node.js',
+    name: 'Node.js',
+    logo: '/icons/nodedotjs.svg',
+    color:
+      'brightness(0) saturate(100%) invert(48%) sepia(66%) saturate(330%) hue-rotate(62deg) brightness(102%) contrast(92%)',
+  },
+  {
+    id: 'express',
+    name: 'Express',
+    logo: '/icons/express.svg',
+    color:
+      'brightness(0) saturate(100%) invert(0%) sepia(0%) saturate(3%) hue-rotate(283deg) brightness(97%) contrast(100%)',
+  },
+  {
+    id: 'mongodb',
+    name: 'MongoDB',
+    logo: '/icons/mongodb.svg',
+    color:
+      'brightness(0) saturate(100%) invert(47%) sepia(97%) saturate(318%) hue-rotate(71deg) brightness(96%) contrast(84%)',
+  },
+  {
+    id: 'mongoose',
+    name: 'Mongoose',
+    logo: '/icons/mongoose.svg',
+    color:
+      'brightness(0) saturate(100%) invert(7%) sepia(98%) saturate(5149%) hue-rotate(13deg) brightness(105%) contrast(117%)',
+  },
+  {
+    id: 'jest',
+    name: 'Jest',
+    logo: '/icons/jest.svg',
+    color:
+      'brightness(0) saturate(100%) invert(15%) sepia(100%) saturate(4271%) hue-rotate(347deg) brightness(78%) contrast(95%)',
+  },
+  {
+    id: 'testinglibrary',
+    name: 'TestingLibrary',
+    logo: '/icons/testinglibrary.svg',
+    color:
+      'brightness(0) saturate(100%) invert(41%) sepia(85%) saturate(2905%) hue-rotate(337deg) brightness(87%) contrast(105%)',
+  },
+  {
+    id: 'git',
+    name: 'GIT',
+    logo: '/icons/git.svg',
+    color:
+      'brightness(0) saturate(100%) invert(49%) sepia(80%) saturate(4473%) hue-rotate(343deg) brightness(98%) contrast(92%)',
+  },
+  {
+    id: 'github',
+    name: 'GitHub',
+    logo: '/icons/github.svg',
+    color:
+      'brightness(0) saturate(100%) invert(7%) sepia(8%) saturate(206%) hue-rotate(314deg) brightness(97%) contrast(95%)',
+  },
+  {
+    id: 'firebase',
+    name: 'Firebase',
+    logo: '/icons/firebase.svg',
+  },
+  {
+    id: 'npm',
+    name: 'NPM',
+    logo: '/icons/npm.svg',
+    color:
+      'brightness(0) saturate(100%) invert(34%) sepia(35%) saturate(3902%) hue-rotate(345deg) brightness(88%) contrast(78%)',
+  },
+
+  {
+    id: 'dotenv',
+    name: 'DOTENV',
+    logo: '/icons/dotenv.svg',
+    color:
+      'brightness(0) saturate(100%) invert(87%) sepia(70%) saturate(3789%) hue-rotate(325deg) brightness(98%) contrast(88%)',
+  },
+];
+
+export const projectsData = [
+  {
+    description: 'Collaboration Video and Chat App',
+    backgroundImage: '',
+    liveURL:
+      'https://connectify-8ikv2t83m-borislavs-projects-3027afe4.vercel.app/',
+
+    github: 'https://github.com/borislavpachev/Messenger-App-Upgrade',
+  },
+];
