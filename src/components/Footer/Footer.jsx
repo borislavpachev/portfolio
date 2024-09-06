46f3aae6d46dbcac5176ab302dc739c474b51f28
borislavpachev <borislav.pachev@gmail.com>
2024-09-06 10:17:43 +0300

    Footer better responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import SocialLinks from "../SocialLinks/SocialLinks";
+import SocialLinks from '../SocialLinks/SocialLinks';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex items-center justify-evenly bg-blue-950/75 p-10 text-xl">
-      <SocialLinks/>
-      <div>{`Copyright © ${currentYear} Borislav Pachev. All rights reserved.`}</div>
+    <footer className="flex flex-col md:flex-row mt-10 items-center justify-evenly bg-blue-950/75 p-10 text-xl">
+      <SocialLinks />
+      <div className="mt-5 md:mt-0">{`Copyright © ${currentYear} Borislav Pachev. All rights reserved.`}</div>
     </footer>
   );
 }
