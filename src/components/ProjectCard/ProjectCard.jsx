dc450d350e28b5a0e713d2f38357b59d044c6bd4
borislavpachev <borislav.pachev@gmail.com>
2024-09-06 10:18:05 +0300

    Project card component

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -2,21 +2,49 @@ import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import AnimatedCard from '../AnimatedCard/AnimatedCard';
 
-export default function ProjectCard({ data }) {
+export default function ProjectCard({ project }) {
   return (
-    <div className="rounded-3xl max-w-sm bg-blue-950 p-3">
-      <p>{data.description}</p>
-      <div className="flex items-center justify-center space-x-3">
-        <Button className={`${!data.liveURL && 'hidden'}`}>Live</Button>
-        <Button>
-          <FontAwesomeIcon size="xl" icon={faGithub} />
-        </Button>
+    <div
+      className="group relative flex flex-col items-center justify-center rounded-3xl hover:shadow-2xl
+     shadow-blue-950 p-2 m-2 bg-white text-xl"
+    >
+      <img
+        title={`${project.name}`}
+        className={`w-[250px] h-[250px] bg-blue-950 object-contain p-2 ${project.backgroundColor} rounded-3xl shadow-lg shadow-blue-950 hover:scale-110`}
+        src={project.backgroundImage}
+        alt={project.name}
+      />
+      <p className="text-blue-950 mt-2">{project.name}</p>
+      <div
+        className="flex flex-col w-full h-full absolute rounded-3xl bottom-0 left-0 right-0 
+        transition duration-500 ease-in-out space-y-5 items-center justify-center
+        bg-blue-300 opacity-0 group-hover:opacity-100
+        border-b-rounded-3xl shadow-2xl"
+      >
+        <p className="text-semibold text-center text-blue-950 text-balance">
+          {project.description}
+        </p>
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <AnimatedCard>
+            <a href={project.liveURL} target="_blank">
+              <Button className={`${!project.liveURL && 'hidden'}`}>
+                Live
+              </Button>
+            </a>
+          </AnimatedCard>
+          <a href={project.github} target="_blank">
+            <Button>
+              <FontAwesomeIcon size="xl" icon={faGithub} />
+            </Button>
+          </a>
+        </div>
       </div>
     </div>
   );
 }
 
 ProjectCard.propTypes = {
-  data: PropTypes.any,
+  project: PropTypes.any,
 };
