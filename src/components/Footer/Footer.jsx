f5d3f908467825006114aa818d240ed822cf7be9
borislavpachev <borislav.pachev@gmail.com>
2024-09-07 18:48:50 +0300

    better UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex flex-col md:flex-row mt-10 items-center justify-evenly bg-blue-950/75 p-10 text-xl">
+    <footer className="flex flex-col md:flex-row mt-20 items-center justify-evenly bg-blue-950/75 p-10 text-xl">
       <SocialLinks />
       <div className="mt-5 md:mt-0">{`Copyright © ${currentYear} Borislav Pachev. All rights reserved.`}</div>
     </footer>
