16313f41243974e5befa24e0eba77a7a12570373
borislavpachev <borislav.pachev@gmail.com>
2024-09-07 18:47:33 +0300

    sections with better code structure

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,5 +1,5 @@
-export { default as AboutMe } from './AboutMe/AboutMe';
-export { default as Contact } from './Contact/Contact';
 export { default as Hero } from './Hero/Hero';
-export { default as Projects } from './Projects/Projects';
+export { default as AboutMe } from './AboutMe/AboutMe';
 export { default as TechStack } from './TechStack/TechStack';
+export { default as Projects } from './Projects/Projects';
+export { default as Contact } from './Contact/Contact';
