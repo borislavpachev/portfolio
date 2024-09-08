52158ab7fcd3677136f4f94d6f33902d8a65004c
borislavpachev <borislav.pachev@gmail.com>
2024-09-08 11:43:57 +0300

    Responsiveness on smaller screens fix

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -18,18 +18,17 @@ export default function HeroSection() {
 
   return (
     <section className="flex flex-col md:flex-row max-w-5xl m-auto my-0 md:my-16 items-center justify-center md:space-x-16">
-      
       <div className="max-w-lg relative flex flex-col justify-center rounded-2xl space-y-8 m-12">
-        <p className="text-5xl tracking-wider text-white/50">
+        <p className="text-3xl md:text-5xl tracking-wide text-white/50">
           Hello, World!{' '}
           <span className="absolute z-30 text-white" ref={greetingHeroHandRef}>
             👋
           </span>
         </p>
-        <h1 className="font-bold text-6xl -tracking-wider">
+        <h1 className="font-bold text-4xl md:text-6xl -tracking-wider">
           {`I'm Borislav Pachev`}
         </h1>
-        <div className="flex items-center space-x-1 text-5xl tracking-wider">
+        <div className="flex items-center space-x-1 text-3xl md:text-5xl tracking-wide">
           <div
             className="bg-blue-950 ease-in rounded-b-lg p-3 -rotate-6 cursor-pointer hover:rotate-0 
             transition duration-300"
@@ -39,16 +38,15 @@ export default function HeroSection() {
           <div className="p-1">Developer</div>
         </div>
       </div>
-      
+
       <div className="h-auto max-w-[450px]">
         <img
           src="/avatar.png"
-          loading='lazy'
+          loading="lazy"
           className="w-full bg-white rounded-3xl shadow-2xl shadow-black/90"
           alt="hero"
         />
       </div>
-      
     </section>
   );
 }
