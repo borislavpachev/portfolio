d6463d76a76a27181aef27b1784ff0fa5bae738e
borislavpachev <borislav.pachev@gmail.com>
2024-09-08 11:43:03 +0300

    CSS Fixes

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -5,10 +5,9 @@ import PropTypes from 'prop-types';
 
 export default function AboutMe({ aboutRef }) {
   return (
-    <Section scrollRef={aboutRef}>
+    <Section scrollRef={aboutRef} className="text-2xl">
       <SectionTitle>About Me</SectionTitle>
-      <div className="max-w-5xl flex items-center shadow-2xl text-2xl bg-white/10 rounded-3xl">
-        
+      <div className="max-w-5xl flex items-center shadow-2xl bg-white/10 rounded-3xl">
         <div className="hidden h-[500px] w-1/3 md:flex">
           <img
             className="h-full object-cover rounded-3xl rounded-r-none"
@@ -21,17 +20,16 @@ export default function AboutMe({ aboutRef }) {
           <div className="absolute top-0 left-7 text-4xl md:text-5xl opacity-50">
             <FontAwesomeIcon icon={faQuoteRight} />
           </div>
-          {`I'm a web developer based in Sofia, always eager for new challenges.
+          <p>{`I'm a web developer based in Sofia, always eager for new challenges.
           Programming has become my passion, turning each project into an
           adventure filled with discovery, personal growth, and the satisfaction
           of creating something new. I love the good company of friends and
           enjoy exploring nature with my camera fully charged or setting out for
-          a winter hike.`}
+          a winter hike.`}</p>
           <div className="absolute bottom-0 right-10 text-4xl md:text-5xl opacity-50">
             <FontAwesomeIcon icon={faQuoteRight} />
           </div>
         </div>
-        
       </div>
     </Section>
   );
