e7e7f790db20b6f962020943ae326228d57d3b80
borislavpachev <borislav.pachev@gmail.com>
2024-09-09 10:34:03 +0300

    Smaller section title on smaller screens

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export default function SectionTitle({ children }) {
-  return <h1 className="text-5xl md:text-6xl mt-10 -tracking-wider">{children}</h1>;
+  return <h1 className="text-4xl md:text-6xl mt-10 -tracking-wider">{children}</h1>;
 }
 
 SectionTitle.propTypes = {
